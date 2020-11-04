d506b2e487e487c6241a166c6416fc82ff56049a
John Sommerville <jsommerville@untangle.com>
2020-11-04 16:49:48 -0700

    simplify run script to check params if they exist, add concept of a results_server for deploying results to

--- run.sh
+++ run.sh
@@ -12,6 +12,11 @@ REMOTE_SERVER_USER=root
 REMOTE_CLIENT=""
 REMOTE_CLIENT_PORT=22
 REMOTE_CLIENT_USER=root
+
+RESULTS_SERVER=""
+RESULTS_SERVER_PORT=22
+RESULTS_SERVER_USER=root
+
 for arg in "$@"
 do
     case $arg in
@@ -45,24 +50,53 @@ do
         shift
         shift
         ;;
+        -rs|--resultserver)
+        RESULTS_SERVER="$2"
+        shift
+        shift
+        ;;
+        -ru|--resultserveruser)
+        RESULTS_SERVER_USER="$2"
+        shift
+        shift
+        ;;
+        -rp|--resultserverport)
+        RESULTS_SERVER_PORT="$2"
+        shift
+        shift
+        ;;
+        
     esac
 done
 
-# Create the remote contexts using params
-docker context create flent_remote_server --docker "host=ssh://$REMOTE_SERVER_USER@$REMOTE_SERVER:$REMOTE_SERVER_PORT"
-docker context create flent_remote_client --docker "host=ssh://$REMOTE_CLIENT_USER@$REMOTE_CLIENT:$REMOTE_CLIENT_PORT"
+if [ -n "$RESULTS_SERVER" ]
+then
+    echo "Setting up results server..."
+fi
 
-# For some reason we need to set these as default before docker-compose will use them
-# see: https://github.com/docker/compose/issues/7434
-docker context use flent_remote_server
-docker context use flent_remote_client
+if [ -n "$REMOTE_SERVER" ]
+then
+    echo "Configuring performance server..."
+    docker context create perf_remote_server --docker "host=ssh://$REMOTE_SERVER_USER@$REMOTE_SERVER:$REMOTE_SERVER_PORT"
+    docker context use perf_remote_server
+    echo "Deploying performance server..."
+    docker-compose -f docker-compose.yml --context perf_remote_server up -d --build perf-server
+    docker context rm perf_remote_server
 
     echo $REMOTE_SERVER > perf-server-location
+else
+    echo netperf-west.bufferbloat.net > perf-server-location
+fi
 
-# Call compose with the context of the server and client, using the appropriate builds
-docker-compose -f docker-compose.yml --context flent_remote_server up -d --build flent-server
-docker-compose -f docker-compose.yml --context flent_remote_client up --build flent-client
+if [ -n "$REMOTE_CLIENT" ]
+then
+    echo "Configuring performance client..."
+    docker context create perf_remote_client --docker "host=ssh://$REMOTE_CLIENT_USER@$REMOTE_CLIENT:$REMOTE_CLIENT_PORT"
+    docker context use perf_remote_client
+    echo "Deploying performance client..."
+    docker-compose -f docker-compose.yml --context perf_remote_client up --build perf-client
+    docker context rm perf_remote_client
+fi
 
-# Remove the contexts
-docker context rm flent_remote_server
-docker context rm flent_remote_client
+# Set context back to default, just in case
+docker context use default
