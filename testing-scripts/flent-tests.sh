9b7321a57cd5a61a6f3dcb8664aa914f6b80d5de
John Sommerville <jsommerville@untangle.com>
2020-11-04 21:21:01 -0700

    fix flent calls to use -l instead of -L

--- testing-scripts/flent-tests.sh
+++ testing-scripts/flent-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-flent rrul -p all_scaled -L 60 -H $PERF_SRV -o rrul_all_scaled.png
-flent tcp_1up -p totals -L 60 -H $PERF_SRV -o tcp_up_totals.png
-flent tcp_1down -p totals -L 60 -H $PERF_SRV -o tcp_down_totals.png
-flent tcp_bidirectional -p totals -L 60 -H $PERF_SRV -o tcp_bidirectional_totals.png
-flent qdisc_stats -L 60 -H $PERF_SRV -o qdisc_stats.png
+flent rrul -p all_scaled -l 60 -H $PERF_SRV -o rrul_all_scaled.png
+flent tcp_1up -p totals -l 60 -H $PERF_SRV -o tcp_up_totals.png
+flent tcp_1down -p totals -l 60 -H $PERF_SRV -o tcp_down_totals.png
+flent tcp_bidirectional -p totals -l 60 -H $PERF_SRV -o tcp_bidirectional_totals.png
+flent qdisc_stats -l 60 -H $PERF_SRV -o qdisc_stats.png
