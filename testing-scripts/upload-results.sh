b79e1ff218e46531ff0f25ae3a5ceadc9a9af32b
John Sommerville <jsommerville@untangle.com>
2020-11-05 16:51:03 -0700

    upload results to the proper directory

--- testing-scripts/upload-results.sh
+++ testing-scripts/upload-results.sh
@@ -4,4 +4,4 @@
 #
 
 #Upload all results with datestamp to $RES_SRV using $RES_PORT and maybe a $RES_USER one day?
-rsync -rvz $1 $RES_SSH_USER@$RES_SRV:/data/$1/
+rsync -rvz $1 $RES_SSH_USER@$RES_SRV:/data/
