70a476b0f32af2fe272e77bcb2f12269dc3b87c6
John Sommerville <jsommerville@untangle.com>
2020-11-10 12:31:07 -0700

    only configure SSH config if RES_SRV exists

--- testing-scripts/setup-ssh.sh
+++ testing-scripts/setup-ssh.sh
@@ -2,11 +2,14 @@
 #
 # Creates the SSH config when needed
 #
-echo -en '\n' >> /root/.ssh/config
-echo "Host ${RES_SRV}" >> /root/.ssh/config
-echo -e "\tHostName ${RES_SRV}" >> /root/.ssh/config
-echo -e "\tPort $RES_SSH_PORT" >> /root/.ssh/config
-echo -e "\tUser $RES_SSH_USER" >> /root/.ssh/config
-echo -e "\tIdentityFile /root/.ssh/id_perf">>/root/.ssh/config
-echo -e "\tStrictHostKeyChecking no">>/root/.ssh/config
-chmod 400 /root/.ssh/config
+if [ -n "$RES_SRV" ]
+then
+    echo -en '\n' >> /root/.ssh/config
+    echo "Host ${RES_SRV}" >> /root/.ssh/config
+    echo -e "\tHostName ${RES_SRV}" >> /root/.ssh/config
+    echo -e "\tPort $RES_SSH_PORT" >> /root/.ssh/config
+    echo -e "\tUser $RES_SSH_USER" >> /root/.ssh/config
+    echo -e "\tIdentityFile /root/.ssh/id_perf">>/root/.ssh/config
+    echo -e "\tStrictHostKeyChecking no">>/root/.ssh/config
+    chmod 400 /root/.ssh/config
+fi
