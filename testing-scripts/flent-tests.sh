0b4858f6532af2cfc0f6a52e339e1faa66fc701d
John Sommerville <jsommerville@untangle.com>
2020-11-10 09:48:16 -0700

    cleanup flent scripts after changing netserver network settings

--- testing-scripts/flent-tests.sh
+++ testing-scripts/flent-tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd $1
 #Change these to look like this:
-flent rrul -p all_scaled -l 60 -H 0.0.0.0 -o rrul_all_scaled.png --control-host $PERF_SRV
-flent tcp_1up -p totals -l 60 -H 0.0.0.0 -o tcp_up_totals.png --control-host $PERF_SRV
-flent tcp_1down -p totals -l 60 -H 0.0.0.0 -o tcp_down_totals.png --control-host $PERF_SRV
-flent tcp_bidirectional -p totals -l 60 -H 0.0.0.0 -o tcp_bidirectional_totals.png --control-host $PERF_SRV
-flent qdisc_stats -l 60 -H 0.0.0.0 -o qdisc_stats.png --control-host $PERF_SRV
+flent rrul -p all_scaled -l 60 -H $PERF_SRV -o rrul_all_scaled.png
+flent tcp_1up -p totals -l 60 -H $PERF_SRV -o tcp_up_totals.png
+flent tcp_1down -p totals -l 60 -H $PERF_SRV -o tcp_down_totals.png
+flent tcp_bidirectional -p totals -l 60 -H $PERF_SRV -o tcp_bidirectional_totals.png
+flent qdisc-stats -l 60 -H $PERF_SRV -o qdisc_stats.png
