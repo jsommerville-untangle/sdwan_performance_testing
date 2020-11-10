db656f2759d3c1bf06f388b93723555c94c173f0
John Sommerville <jsommerville@untangle.com>
2020-11-10 08:18:30 -0700

    only upload results when RES_SRV exists

--- testing-scripts/upload-results.sh
+++ testing-scripts/upload-results.sh
@@ -3,5 +3,8 @@
 # uploads all test results from the working directory
 #
 
-#Upload all results with datestamp to $RES_SRV using $RES_PORT and maybe a $RES_USER one day?
-rsync -rvz $1 $RES_SSH_USER@$RES_SRV:/data/
+if [ -n "$RES_SRV" ]
+then
+    #Upload all results with datestamp to $RES_SRV using $RES_PORT and maybe a $RES_USER one day?
+    rsync -rvz $1 $RES_SSH_USER@$RES_SRV:/data/
+fi
