ed10ff4507f7e3ad0a9d1446b1bc23ecf5daccac
John Sommerville <jsommerville@untangle.com>
2020-11-10 15:14:38 -0700

    create results.txt file with summary of results

--- testing-scripts/flent-tests.sh
+++ testing-scripts/flent-tests.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 cd $1
-#Change these to look like this:
+
 flent rrul -p all_scaled -l 60 -H $PERF_SRV -o rrul_all_scaled.png
 flent tcp_1up -p totals -l 60 -H $PERF_SRV -o tcp_up_totals.png
 flent tcp_1down -p totals -l 60 -H $PERF_SRV -o tcp_down_totals.png
 flent tcp_bidirectional -p totals -l 60 -H $PERF_SRV -o tcp_bidirectional_totals.png
 flent qdisc-stats -l 60 -H $PERF_SRV -o qdisc_stats.png
+
+#Iterate the results and append them into the results.txt file
+for f in *.flent.gz
+do
+    flent $f >> results.txt
+done
