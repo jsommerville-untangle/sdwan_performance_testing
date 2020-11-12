7258c469972169efa3becc506548d911f6c20b6f
John Sommerville <jsommerville@untangle.com>
2020-11-12 15:18:58 -0700

    update orchestrator entrypoint to be run-perf-testing script

--- orchestration/run-perf-testing.py
+++ orchestration/run-perf-testing.py
@@ -2,6 +2,7 @@ import json
 import subprocess
 
 # Parse the json configuration files
-
+print("Parsing supplied config files...")
 # For each json config item, deploy associated image config and call run.sh to execute the tests
-
+print("Calling subprocess run-tests.sh script...")
+subprocess.call(['run-tests.sh'], shell=True)
